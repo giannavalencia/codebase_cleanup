fce9bb1079c0101d74539fe8632cd533a04ee214
giannavalencia <gvalealb@gmail.com>
2022-04-03 23:26:09 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,16 @@ def to_usd(my_price):
     Invoke like this: to_usd(99.999)
     """
     return '${:,.2f}'.format(my_price)
+<<<<<<< HEAD
+=======
+
+if __name__=="__main__":
+    # nesting code in the main condition will
+    #allow other scripts to cleanly import
+    #without running this code
+
+    #this code still gets run when we invoke the script from the command line
+    price = input("Please choose a price like 4.9999")
+
+    print(to_usd(float(price)))
+>>>>>>> simpler
